f1339aedb3516c4452336aad0c370bd274db493d
Sarah Ribeiro <sarahribeiro@Sarahs-MacBook-Pro.local>
2024-07-28 16:47:05 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactSectionComponent } from "./contact-section/contact-section.component";
+import { SobreSectionComponent } from "./sobre-section/sobre-section.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { ServicosSectionComponent } from "./servicos-section/servicos-section.component";
+import { DivNumbersComponent } from "./div-numbers/div-numbers.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, ContactSectionComponent, SobreSectionComponent, HeroSectionComponent, ServicosSectionComponent, DivNumbersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
